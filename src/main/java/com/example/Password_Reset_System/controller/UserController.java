cfe8cc36f9756220dbe37fe7665b25b66c911d9a
IN_MOURITECH\aryang <aryang.in@mouritech.com>
2025-12-28 21:53:18 +0530

    feat/Added the email functionality and integration with frontend

--- src/main/java/com/example/Password_Reset_System/controller/UserController.java
+++ src/main/java/com/example/Password_Reset_System/controller/UserController.java
@@ -4,11 +4,9 @@ import com.example.Password_Reset_System.dto.LoginDto;
 import com.example.Password_Reset_System.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/auth/user")
 public class UserController {
